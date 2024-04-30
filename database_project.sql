02516ac6daef9efa0e3f1a42ef77571806cf9358
keita.watanabe <keita.watanabe@g.dreamcareer.co.jp>
2024-04-30 11:51:31 +0900

    データを追加しました

--- database_project.sql
+++ database_project.sql
@@ -6,3 +6,10 @@ CREATE TABLE products (
     UnitPrice DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (ProductCode)
 );
+
+INSERT INTO products (ProductCode, ProductName, UnitPrice) VALUES
+('P1001', 'Product 1', 10.00),
+('P1002', 'Product 2', 15.50),
+('P1003', 'Product 3', 8.75),
+('P1004', 'Product 4', 12.00),
+('P1005', 'Product 5', 9.25);
