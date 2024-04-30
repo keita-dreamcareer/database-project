17bffae2e2964b11232c89bbd94271230cbed45d
keita.watanabe <keita.watanabe@g.dreamcareer.co.jp>
2024-04-30 12:07:43 +0900

    mysqlでトランザクション処理の一連

--- database_project.sql
+++ database_project.sql
@@ -13,3 +13,17 @@ INSERT INTO products (ProductCode, ProductName, UnitPrice) VALUES
 ('P1003', 'Product 3', 8.75),
 ('P1004', 'Product 4', 12.00),
 ('P1005', 'Product 5', 9.25);
+
+START TRANSACTION;
+
+INSERT INTO products (ProductCode, ProductName, UnitPrice) VALUES
+('P1006', 'Product 6', 14.95);
+
+ROLLBACK;
+
+INSERT INTO products (ProductCode, ProductName, UnitPrice) VALUES
+('P1006', 'Product 6', 14.95);
+
+COMMIT;
+
+
