5e450ce77263dc49c3a820d731a25f70a8c878ab
keita.watanabe <keita.watanabe@g.dreamcareer.co.jp>
2024-04-30 11:48:12 +0900

    productテーブル作成

--- database_project.sql
+++ database_project.sql
@@ -1,2 +1,8 @@
 CREATE DATABASE day5;
 
+CREATE TABLE products (
+    ProductCode VARCHAR(10) NOT NULL,
+    ProductName VARCHAR(255) NOT NULL,
+    UnitPrice DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (ProductCode)
+);
